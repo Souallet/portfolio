e1814f8be4268849c63c2f9e7bad392df49883eb
Sébastien Ouallet <sebastien.ouallet@gmail.com>
2020-08-19 22:45:48 +0200

    Added TailwindCSS

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: {
+    enabled: true,
+    content: ["./src/**/*.html", "./src/**/*.scss"],
+  },
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+};
