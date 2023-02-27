c386c743468099b2c629df9946b85ad4199bd4b9
Sébastien Ouallet <sebastien.ouallet@gmail.com>
2023-02-27 11:06:31 +0100

    feat: Update Experiences section

--- src/components/sections/Home/Experiences.tsx
+++ src/components/sections/Home/Experiences.tsx
@@ -1,3 +1,48 @@
+import SectionTitle from '@/components/templates/SectionTitle';
+import Image from 'next/image';
+
+import CadicServicesLogo from '../../../../public/images/cadic_logo.png';
+import DisneylandParisLogo from '../../../../public/images/disneyland_paris_logo.png';
+import LeboncoinLogo from '../../../../public/images/leboncoin_logo.png';
+import OpenclassRoomsLogo from '../../../../public/images/openclassrooms_logo.png';
+
+const companies = [
+  {
+    name: 'Leboncoin',
+    logo: LeboncoinLogo,
+  },
+  {
+    name: 'OpenClassRooms',
+    logo: OpenclassRoomsLogo,
+  },
+  {
+    name: 'Cadic Services',
+    logo: CadicServicesLogo,
+  },
+  {
+    name: 'Disneyland Paris',
+    logo: DisneylandParisLogo,
+  },
+];
+
 export default function ExperiencesSection() {
-  return <section>ExperiencesSection</section>;
+  const renderCompanies = (): React.ReactNode => {
+    return companies.map((c, i) => (
+      <Image
+        key={i}
+        className="grayscale hover:grayscale-0 transition duration-300 ease-in-out "
+        src={c.logo}
+        alt={`Logo ${c.name}`}
+      />
+    ));
+  };
+
+  return (
+    <section className="my-16 px-8 md:px-12 lg:px-16">
+      <SectionTitle>{`Ils m'ont fait confiance`}</SectionTitle>
+      <div className="grid gap-12 grid-cols-1 sm:grid-cols-2 md:grid-cols-4 items-center">
+        {renderCompanies()}
+      </div>
+    </section>
+  );
 }
--- src/components/templates/SectionTitle.tsx
+++ src/components/templates/SectionTitle.tsx
@@ -1,8 +1,14 @@
 type Props = {
-  title: string;
+  children: string;
   className?: string;
 };
 
-export default function SectionTitle({ title, className = '' }: Props) {
-  return <h1 className={`${className}`}>{title}</h1>;
+export default function SectionTitle({ children, className = '' }: Props) {
+  return (
+    <h2
+      className={`text-center text-slate-900 text-3xl tracking-tight font-extrabold sm:text-5xl dark:text-white ${className}`}
+    >
+      {children}
+    </h2>
+  );
 }
