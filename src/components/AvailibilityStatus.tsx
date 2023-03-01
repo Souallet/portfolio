c6cf348bbd579ef93b4d783555c9c626e97e34e3
Sébastien Ouallet <sebastien.ouallet@gmail.com>
2023-03-01 14:02:37 +0100

    feat: add availibity status component

---
+++ src/components/AvailibilityStatus.tsx
@@ -0,0 +1,24 @@
+import Card from '@/components/templates/Card';
+
+type Props = {
+  available: boolean;
+  description: string;
+};
+
+export default function AvailibilityStatus({ available, description }: Props) {
+  const renderStatusDot = (): React.ReactNode => {
+    return (
+      <i
+        className={`h-5 w-5 rounded-full ${
+          available ? 'bg-green-500' : 'bg-red-500'
+        }`}
+      />
+    );
+  };
+  return (
+    <Card className="py-3 px-5 flex items-center gap-4 rounded-full">
+      {renderStatusDot()}
+      <div>Actuellement {available ? 'disponible' : 'indisponible'}</div>
+    </Card>
+  );
+}
--- src/components/sections/Home/Hero.tsx
+++ src/components/sections/Home/Hero.tsx
@@ -1,3 +1,4 @@
+import AvailibilityStatus from '@/components/AvailibilityStatus';
 import MainTitle from '@/components/templates/MainTitle';
 
 export default function HeroSection() {
@@ -7,7 +8,15 @@ export default function HeroSection() {
       <p className="mt-6 text-lg text-slate-600 text-center max-w-3xl mx-auto dark:text-slate-400">
         {`Développeur FullStack, je réalise des applications web et mobile avec des technologies adaptées aux besoins techniques et fonctionnels afin de rendre l'expérience utilisateur optimale.`}
       </p>
-      <div className="mt-6 sm:mt-10 flex justify-center space-x-6 text-sm">
+
+      <div className="flex flex-col sm:flex-row justify-evenly gap-8 md:gap-12 xl:gap-16 my-4 md:my-8">
+        <AvailibilityStatus
+          available={false}
+          description={`Mission longue durée`}
+        />
+      </div>
+
+      <div className="flex justify-center space-x-6 text-sm">
         <a
           className="bg-slate-900 hover:bg-slate-700 focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 text-white font-semibold h-12 px-6 rounded-lg w-full flex items-center justify-center sm:w-auto dark:bg-sky-500 dark:highlight-white/20 dark:hover:bg-sky-400"
           href="/docs/installation"
