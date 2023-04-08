78f9c9296616fce43bff7ca2a40c5c4eae2b26e3
Sébastien Ouallet <sebastien.ouallet@gmail.com>
2023-04-08 21:24:17 +0200

    feat(education): update section

--- src/components/sections/Home/Educations.tsx
+++ src/components/sections/Home/Educations.tsx
@@ -1,9 +1,52 @@
+import Card from '@/components/templates/Card';
 import SectionTitle from '@/components/templates/SectionTitle';
+
+const educations = [
+  {
+    school: 'Infosup',
+    degree: 'Brevet Technicien Supérieur',
+    name: 'Services Informatiques aux Organisations',
+    specialisation: 'Solutions Logicielles et Applications Métiers',
+    location: 'Paris, France',
+    startedAt: '2015',
+    endedAt: '2017',
+    description: '',
+  },
+  {
+    school: 'Ynov Informatique',
+    degree: 'Mastère',
+    name: "Expert Développement Web et Systèmes d'Information",
+    specialisation: '',
+    location: 'Nanterre, France',
+    startedAt: '2018',
+    endedAt: '2020',
+    description: '',
+  },
+];
 
 export default function EducationsSection() {
   return (
     <section className="my-32 px-8 md:px-12 lg:px-16">
       <SectionTitle>{`Mes Formations`}</SectionTitle>
+
+      <div className="grid md:grid-cols-2 sm:gap-x-[10%] md:gap-x-[5%] gap-y-10 my-24 ">
+        {educations.map((e, i) => (
+          <Card key={i} className="flex flex-col p-4 md:p-8 gap-4">
+            <div className="flex justify-between w-full">
+              <h3 className="text-slate-900 dark:text-slate-200 font-bold text-xl">
+                {e.degree}
+              </h3>
+              <span className="text-slate-500 dark:text-slate-400 ">
+                @{e.school}
+              </span>
+            </div>
+            <p className="text-slate-900 dark:text-slate-300">{e.name}</p>
+            <p className="text-slate-900 dark:text-slate-300">
+              {e.startedAt} - {e.endedAt}
+            </p>
+          </Card>
+        ))}
+      </div>
     </section>
   );
 }
--- src/components/templates/Card.tsx
+++ src/components/templates/Card.tsx
@@ -7,7 +7,7 @@ type Props = {
 export default function Card({ children, className = '' }: Props) {
   return (
     <div
-      className={`md:flex bg-gray-50 rounded-xl dark:bg-slate-800 ${className}`}
+      className={`md:flex rounded-md border border-gray-200 dark:border-gray-700 dark:bg-slate-800 ${className}`}
     >
       {children}
     </div>
