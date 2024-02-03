930abf0c3ee50247b7c23b28cccb97e446248bbe
Sébastien Ouallet <sebastien.ouallet@gmail.com>
2024-02-03 10:26:51 +0100

    feat: add form contact ui

---
+++ src/components/forms/contact.tsx
@@ -0,0 +1,126 @@
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui//form';
+import { Button } from '@/components/ui/button';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { z } from 'zod';
+import { Input } from '../ui/input';
+import { Textarea } from '../ui/textarea';
+
+const formSchema = z.object({
+  name: z.string({ required_error: 'Veuillez spécifier votre nom' }).min(3, {
+    message: 'Votre nom doit contenir au moins 3 caractères.',
+  }),
+  email: z
+    .string({ required_error: 'Veuillez spécifier votre adresse e-mail' })
+    .min(1, { message: 'Votre adresse e-mail est requise.' })
+    .email({
+      message: 'Adresse e-mail invalide',
+    }),
+  subject: z
+    .string({ required_error: 'Veuillez spécifier le sujet de votre demande' })
+    .min(2, {
+      message: 'Votre sujet doit contenir au moins 10 caractères.',
+    }),
+  message: z
+    .string({ required_error: 'Veuillez développer votre demande' })
+    .min(50, {
+      message: 'Votre sujet doit contenir au moins 50 caractères.',
+    }),
+});
+
+export default function ContactForm() {
+  const onSubmit: SubmitHandler<z.infer<typeof formSchema>> = (data) =>
+    console.log(data);
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+  });
+
+  return (
+    <Form {...form}>
+      <form
+        className="grid gap-6 grid-cols-1 md:grid-cols-2 max-w-[750px]"
+        onSubmit={form.handleSubmit(onSubmit)}
+      >
+        <FormField
+          control={form.control}
+          name="name"
+          render={({ field }) => (
+            <FormItem className="flex flex-col col-span-1">
+              <FormLabel>Votre nom</FormLabel>
+              <FormControl>
+                <Input placeholder="Sébastien O." {...field} />
+              </FormControl>
+              <FormMessage className="text-xs" />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem className="flex flex-col col-span-1">
+              <FormLabel>Votre address e-mail</FormLabel>
+              <FormControl>
+                <Input placeholder="sebastien.o@email.com" {...field} />
+              </FormControl>
+              <FormMessage className="text-xs" />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="subject"
+          render={({ field }) => (
+            <FormItem className="flex flex-col md:col-span-2">
+              <FormLabel>Le sujet de votre message</FormLabel>
+              <FormControl>
+                <Input
+                  placeholder="Création de site web, développement de plugin, etc..."
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage className="text-xs" />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="message"
+          render={({ field }) => (
+            <FormItem className="flex flex-col md:col-span-2">
+              <FormLabel>Votre message</FormLabel>
+              <FormControl>
+                <Textarea
+                  className=""
+                  placeholder="Je vous contacte pour ..."
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage className="text-xs" />
+            </FormItem>
+          )}
+        />
+
+        <Button
+          disabled={form.formState.isSubmitSuccessful}
+          variant="secondary"
+          type="submit"
+          className="px-12 w-fit"
+        >
+          {form.formState.isSubmitSuccessful ? 'Message envoyé' : 'Envoyer'}
+        </Button>
+      </form>
+    </Form>
+  );
+}
--- src/components/sections/Home/Contact.tsx
+++ src/components/sections/Home/Contact.tsx
@@ -1,24 +1,27 @@
-import { siteConfig } from '@/config/site';
-import { Button } from '@ui/button';
-import { Section, SectionDescription, SectionTitle } from '@ui/section';
-import { useRouter } from 'next/navigation';
+import ContactForm from '@/components/forms/contact';
+import {
+  Section,
+  SectionContent,
+  SectionDescription,
+  SectionTitle,
+} from '@ui/section';
 
 export default function ContactSection() {
-  const router = useRouter();
-
-  const redirectToMailTo = () => {
-    router.push(`mailto:${siteConfig.links.mail}`);
-  };
+  // const router = useRouter();
+  // const redirectToMailTo = () => {
+  //   router.push(`mailto:${siteConfig.links.mail}`);
+  // };
 
   return (
     <Section>
       <SectionTitle>{`Restons en contact`}</SectionTitle>
-      <SectionDescription className="flex flex-wrap items-center justify-between">
-        <span className="text-lg md:text-md lg:text-xl xl:text-2xl">
-          Une idée en tête ? Parlons en !
-        </span>
-        <Button onClick={redirectToMailTo}>Envoyez-moi un mail !</Button>
+      <SectionDescription>
+        Une idée en tête ? Parlons en !
+        {/* <Button onClick={redirectToMailTo}>Envoyez-moi un mail !</Button> */}
       </SectionDescription>
+      <SectionContent className="">
+        <ContactForm />
+      </SectionContent>
     </Section>
   );
 }
