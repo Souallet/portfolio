f77c07dd92d7f8b94ab0843ad82bd84caf1071e8
Sébastien Ouallet <sebastien.ouallet@gmail.com>
2024-04-09 12:10:03 +0200

    build: add Docker Image

--- next.config.js
+++ next.config.js
@@ -4,6 +4,8 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
 
-const nextConfig = {};
+const nextConfig = {
+  output: 'standalone',
+};
 
 module.exports = withBundleAnalyzer(nextConfig);
